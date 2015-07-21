06f0aee4d19f9331cfe568598ac6db88625ce0f5
Mohammad Heidari <mhm6626@yahoo.com>
2015-07-21 08:30:17 +0430

    add ignore

--- web.js
+++ web.js
@@ -1,12 +1,12 @@
 var app = require('app');
 
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
 	res.render('main');
 });
 
 require('./routes');
 
-app.listen(3000, function () {
+app.listen(3000, function() {
 	console.log('Server Started...');
 });
