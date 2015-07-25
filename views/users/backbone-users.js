1d584ba48bb3e33e940ceecb4c4b66a40b0b2a36
Mohammad Heidari <mhm6626@yahoo.com>
2015-07-25 11:17:40 +0430

    View -- modify

--- views/users/backbone-users.js
+++ views/users/backbone-users.js
@@ -2,38 +2,41 @@
  * Created by Mohammad on 7/10/15.
  */
 var User = Backbone.Model.extend({
-	urlRoot: '/users'
+    urlRoot: '/users'
 });
 
 var user = new User({
-	email: 'javad@mhm.it',
-	password: '123456'
+    email: 'javad@mhm.it',
+    password: '123456'
 });
 user.save({created: Date.now()});
 
 var DocumentRow = Backbone.View.extend({
 
-	tagName: "form",
+    tagName: "div",
+    className: "div-cls",
+    el: $('#view'),
+    template: _.template($('#users-login-tpl').html()),
 
-	template: _.template($('#users-login-tpl').html()),
+    events: {
+        "click .btn": "btnSave"
+    },
 
-	events: {
-	 "click .btn": "btnSave",
-	 },
+    initialize: function () {
+        //this.$el = $('#view');
+        this.render();
+    },
 
-	initialize: function () {
-		this.$el = $('#view');
-	},
+    render: function () {
+        this.$el.html(this.template(this.model.toJSON()));
+        //$('#view').append(this.el);
+        console.log(this.el);
+    },
 
-	render: function () {
-		this.$el.html(this.template(this.model.toJSON()));
-		return this;
-	},
-
-	btnSave: function (e) {
-		alert(e)
-	}
+    btnSave: function (e) {
+        console.log(this.model.toJSON().email)
+    }
 });
-var v = new DocumentRow({model: user});
-v.render();
-v.btnSave(0320)
+var v = new DocumentRow({
+    model: user
+});
