2990efd89c9532fea5136be4e15a3bc3a1e5acda
Mohammad Heidari <mhm6626@yahoo.com>
2015-07-26 09:19:57 +0430

    no message

--- pc.js
+++ pc.js
@@ -1,29 +1,28 @@
 const fs = require('fs');
 
-function C(y) {
+function isLeap(y) {
 	y %= 128;
 	var x = [0, 4, 8, 12, 16, 20, 24, 28, 33, 37, 41, 45, 49, 53, 57, 61, 66, 70, 74, 78, 82, 86, 90, 95, 99, 103, 107, 111, 115, 119, 123];
-	if (x.indexOf(y) > -1) {
+	if (x.indexOf(y) > -1)
 		return true;
-	}
 	return false;
 }
 
 for (var i = 1; i < 1600; i++) {
 
-	if (C(i)) {
+	if (isLeap(i)) {
 		//console.log(i);
 	}
 }
 
-fs.readFile('./1', function(err, data) {
+fs.readFile('./2', function(err, data) {
 	if (err)
 		throw err;
 	var a = data.toString().split('\n');
 	var oldType, newType, j = 0;
 	oldType = parseInt(a[0]) % 2;
 
-	for (var i = 1; i < a.length - 1; i++) {
+	for (var i = 1; i < a.length; i++) {
 		newType = parseInt(a[i]) % 2;
 		if (oldType != newType) {
 			console.log(a[i] - a[j]);
