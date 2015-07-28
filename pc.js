f919b09bcf984cd58210e6a74bac75fc242b2ec3
Mohammad Heidari <mhm6626@yahoo.com>
2015-07-28 17:14:47 +0430

    Save pc

--- pc.js
+++ pc.js
@@ -8,10 +8,42 @@ function isPersianLeap(y) {
 		return true;
 	return false;
 }
-isPersianLeap(55) 
+//isPersianLeap(55);
+//console.log(31 * 366 + 97 * 365) // 46751
 function isGregorianLeap(y) {
 	return ((y % 4 == 0) && (y % 100 != 0)) || (y % 400 == 0);
 }
+
+function pYear(y) {
+	var num = Math.floor((y - 1) / 128);
+	var i = (y - 1) % 128;
+	var num2 = num * 46751  + 226894 ;
+	while (i > 0) {
+		num2 += 365 ;
+		if (isPersianLeap(i, 0)) {
+			num2 += 1 ;
+		}
+		i--;
+	}
+	return num2;
+}
+
+function p2gYear(d) {
+	var num = Math.floor(d / 146097);
+	var i = d % 146097;
+	var num2 = num * 400;
+	while (i > 0) {
+		i -= 365 ;
+		num2 += 1;
+		if (isGregorianLeap(num2)) {
+			i -= 1 ;
+		}
+		
+	}
+	return [num2,  i + 365];
+}
+
+console.log(p2gYear(pYear(1298)))
 function DaysUpToPersianYear(y) {
 	var num = Math.floor((y - 1) / 128);
 	var i = (y - 1) % 128;
@@ -27,7 +59,7 @@ function DaysUpToPersianYear(y) {
 }
 const OFFSET_DAYS = 226894; // parseInt(621 * 365.2425 + 31 + 28 + 20)
 //var x = 12053; // 9 * 366 + 24 * 365 - 1
-console.log(DaysUpToPersianYear(1394))
+//console.log(DaysUpToPersianYear(1394))
 
 /*fs.readFile('./2', function(err, data) {
 	if (err)
