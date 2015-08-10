1428ec173f5a455a4ef04b9a2fc94e0baf0beb24
Mohammad Heidari <mhm6626@yahoo.com>
2015-08-10 15:26:39 +0430

    Exact Calender(1day / 35M years) + EOL to UNIX

--- pc.js
+++ pc.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 function isPersianLeap(y) {
 	y %= 128;
 	var x = [0, 4, 8, 12, 16, 20, 24, 28, 33, 37, 41, 45, 49, 53, 57, 61, 66, 70, 74, 78, 82, 86, 90, 95, 99, 103, 107, 111, 115, 119, 123];
-	
+
 	if (x.indexOf(y) > -1)
 		return true;
 	return false;
@@ -19,11 +19,11 @@ function isGregorianLeap(y) {
 function pYear(y) {
 	var num = Math.floor((y - 1) / 128);
 	var i = (y - 1) % 128;
-	var num2 = num * 46751  + 226894 ;
+	var num2 = num * 46751 + 226894;
 	while (i > 0) {
-		num2 += 365 ;
+		num2 += 365;
 		if (isPersianLeap(i, 0)) {
-			num2 += 1 ;
+			num2 += 1;
 		}
 		i--;
 	}
@@ -35,25 +35,25 @@ function p2gYear(d) {
 	var i = d % 146097;
 	var num2 = num * 400;
 	while (i > 0) {
-		i -= 365 ;
+		i -= 365;
 		num2 += 1;
 		if (isGregorianLeap(num2)) {
-			i -= 1 ;
+			i -= 1;
 		}
-		
+
 	}
-	return [num2,  i + 365];
+	return [num2, i + 365];
 }
 
-console.log(p2gYear(pYear(1298)))
+//console.log(p2gYear(pYear(1298)))
 function DaysUpToPersianYear(y) {
 	var num = Math.floor((y - 1) / 128);
 	var i = (y - 1) % 128;
-	var num2 = num * 46750  + 226894 ;
+	var num2 = num * 46750 + 226894;
 	while (i > 0) {
-		num2 += 365 ;
+		num2 += 365;
 		if (isPersianLeap(i, 0)) {
-			num2 += 1 ;
+			num2 += 1;
 		}
 		i--;
 	}
@@ -79,3 +79,12 @@ const OFFSET_DAYS = 226894; // parseInt(621 * 365.2425 + 31 + 28 + 20)
 		}
 	}
 });**/
+
+function d(n, m, k) {
+	var I = Math.floor;
+	var c = I(n / 100);
+	var y = n % 100;
+	return (k + 5 * c + y + I(c / 4) + I(y / 4) + I(2.6 * m - 0.2)) % 7;
+}
+
+console.log(d(1987, 11, 24))
